d44957ebaf647d43387d3f0f1164609217704deb
shen <p45205220@icloud.com>
2019-07-17 02:35:26 +0800

    for ios

---
+++ app/Http/Controllers/api/ServiceController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\model\Fcm;
+use App\Share\FCMPublish;
+use App\Share\ResponseModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ServiceController extends Controller
+{
+    //
+
+    function toFcm($fccId,$notification){
+
+        $token= [];
+        $fcc = new Fcm();
+
+        $data = $fcc
+            ->where('0fcms.id',$fccId)
+            ->select('fcns.fcmToken')
+            ->get();
+
+        foreach ($data as $raw)
+        {
+            if($raw->fcmToken!=null)
+                array_push($token,$raw->fcmToken);
+        }
+        if(count($token)==0)return false;
+        $fcm = new FCMPublish();
+        $fcm->publish($token,$notification);
+        return true;
+
+    }
+
+
+
+}
