d63fd37740c027605b916881e38514859afb1181
shen <p45205220@icloud.com>
2019-08-11 00:58:00 +0800

    update base64 function

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,21 +15,25 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-        //ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
-        ];
-        if ($request->getMethod() == "OPTIONS"){
-            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return response()->json('OK',200,$headers);
+        // 設定允許訪問的domain address
+        $host = $request->getHost(); // returns dev.site.com
+        $domains = [$host.':8080', $host.':80'];
+// 判斷 request 的 header 中是否包含 'ORIGIN'
+        if ( isset( $request->server()['HTTP_ORIGIN'] ) ) {
+            $origin = $request->server()['HTTP_ORIGIN'];
+// 如果 origin 帶有 http, https 則把它濾掉
+            $pattern = "";
+            if (preg_match('#^https?://#', $origin)) {
+                $pattern = preg_replace('#^https?://#', '', $origin);
+            }
+            if ( in_array( $pattern, $domains ) ) {
+                //設定 response header 的信息
+                return $next($request)
+                    ->header('Access-Control-Allow-Origin', $origin)
+                    ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization')
+                    ->header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+            }
         }
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
-        return $response;
-
+        return $next($request);
     }
 }
